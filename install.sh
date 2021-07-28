e71f4ccc5a2466b28901226fc1b3fcf9a2259c61
tknr <geishagirlskoume@gmail.com>
2021-07-29 01:46:50 +0900

    modified:   git_add_commit_push.sh 	modified:   install.sh 	renamed:    add_video_title -> set_video_title

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 cd `dirname $0`
 rm /usr/local/bin/convert_to_mp4
 rm /usr/local/bin/downconvert_resolution
-rm /usr/local/bin/add_video_title
+rm /usr/local/bin/set_video_title
 ln -s `pwd`/convert_to_mp4 /usr/local/bin/
 ln -s `pwd`/downconvert_resolution /usr/local/bin/
-ln -s `pwd`/add_video_title /usr/local/bin/
+ln -s `pwd`/set_video_title /usr/local/bin/
 
