c976d27bfe0ab69edef694cb474fbc23a59cf05a
tknr <geishagirlskoume@gmail.com>
2025-02-22 22:15:51 +0900

    u

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd `dirname $0`
 rm /usr/local/bin/convert_to_mp4
-rm /usr/local/bin/downconvert_resolution
+rm /usr/local/bin/downconvert_to_720p
 rm /usr/local/bin/set_video_title
-rm /usr/local/bin/convert_to_h265
 ln -s `pwd`/convert_to_mp4 /usr/local/bin/
-ln -s `pwd`/downconvert_resolution /usr/local/bin/
+ln -s `pwd`/downconvert_to_720p /usr/local/bin/
 ln -s `pwd`/set_video_title /usr/local/bin/
-ln -s `pwd`/convert_to_h265 /usr/local/bin/
